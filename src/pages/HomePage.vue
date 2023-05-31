2cf0b251f30eaa7abc64b58219a797c0dd8d23cc
Yulia <you@example.com>
2023-05-31 23:42:31 +0300

    rabbit added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,8 @@
 <template>
   <h3 class="text-center">Добро пожаловать в книжный магазин</h3>
 
-  <q-img src="~src\assets\forhomepage.jpg" alt="homepageimage" width="100%" height="50%" fit="fill"/>
+  <div>
+    <q-img src="~src\assets\forhomepage.jpg" alt="homepageimage" />
+  </div>
 </template>
+
