2011ca1007c6733e9427d4564dea23c09e322c6d
Yulia <you@example.com>
2023-06-01 02:01:43 +0300

    styles added

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,25 @@
 <template>
-  <h3 class="text-center">Добро пожаловать в книжный магазин</h3>
-
-  <div>
-    <q-img src="~src\assets\forhomepage.jpg" alt="homepageimage" />
-  </div>
+  <q-page>
+    <div class="text-center no-margin q-py-md text-h3">
+      Добро пожаловать в книжный магазин
+    </div>
+    <div class="image_container">
+      <q-img src="~src\assets\forhomepage.jpg" alt="homepageimage" height="73vh" fit="none" />
+    </div>
+  </q-page>
 </template>
+
+<style>
+.text-font {
+  font-family: "Roboto", "-apple-system", "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+  font-weight: 200;
+  color: rgba(0, 0, 0, 0.75);
+}
+.image_container {
+  max-height: 100px!important;
+}
+</style>
+
+
 
